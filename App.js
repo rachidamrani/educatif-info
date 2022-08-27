86528d0304e683016e2ed4c6038c23dd76b42a77
rachidamrani <rachid3alaoui@gmail.com>
2022-08-27 17:10:20 +0100

    add stack navigator

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 import LoginScreen from './screens/LoginScreen'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import WelcomeScreen from './screens/WelcomeScreen'
+
+const Stack = createNativeStackNavigator()
+
+function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          contentStyle: {
+            backgroundColor: '#fff',
+          },
+        }}
+      >
+        <Stack.Screen name='LoginScreen' component={LoginScreen} />
+        <Stack.Screen name='WelcomeScreen' component={WelcomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <LoginScreen />
-      <StatusBar style='auto' />
-    </View>
-  )
+  return <Navigation />
 }
 
 const styles = StyleSheet.create({
