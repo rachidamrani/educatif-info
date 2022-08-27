fb2bd490ce5aeaccb35c5b6dbda8b98337e5291c
rachidamrani <rachid3alaoui@gmail.com>
2022-08-27 14:59:02 +0100

    add logo image

--- App.js
+++ App.js
@@ -17,6 +17,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 5,
+    padding: 20,
   },
 })
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, View, Image, useWindowDimensions } from 'react-native'
+import logo from '../assets/learner.jpg'
 
 const LoginScreen = () => {
+  const { height } = useWindowDimensions()
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View style={styles.container}>
+      <Image
+        source={logo}
+        resizeMode='contain'
+        style={[styles.logo, { height: height * 0.3 }]}
+      />
     </View>
   )
 }
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+    // backgroundColor: 'gray',
+  },
+  logo: {
+    marginVertical: 5,
+  },
+})
