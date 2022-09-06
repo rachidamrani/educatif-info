0025cc9c4ee052678170a5e38a612c0e1bbc90a1
rachidamrani <rachid3alaoui@gmail.com>
2022-09-06 15:55:11 +0100

    display profile infos

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,16 +1,55 @@
 import { useRoute } from '@react-navigation/native'
+import { useLayoutEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { useSelector } from 'react-redux'
+import InfoRow from '../components/InfoRow'
+import { Button, Card } from 'react-native-paper'
 
-const Profile = () => {
+const Profile = ({ navigation }) => {
+  const route = useRoute()
   const { adherents } = useSelector((state) => state)
-  const adherent = adherents.find(
-    (item) => item.id === useRoute().params?.adherentId
-  )
+  const profileId = route.params.profileId
+
+  const adherent = adherents.find((item) => item.id === profileId)
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: `Profile de ${adherent.firstname} ${adherent.lastname} `,
+    })
+  }, [adherent, navigation])
 
   return (
     <View style={styles.container}>
-      <Text>Profile de {adherent.firstname} </Text>
+      <Card style={styles.cardProfile}>
+        <InfoRow
+          label='Nom Complet'
+          info={`${adherent.firstname} ${adherent.lastname}`}
+        />
+        <InfoRow label='Niveau' info={adherent.level} />
+        <InfoRow label='Date de naissance' info={adherent.dateOfBirth} />
+        <InfoRow label='Résponsable légal' info={adherent.responsible} />
+        <InfoRow label="Date d'inscription" info={adherent.registrationDate} />
+        <InfoRow label='Téléphone' info={adherent.phone} />
+
+        <View style={styles.controls}>
+          <Button
+            icon='account-edit'
+            mode='contained'
+            onPress={() => console.log('Pressed')}
+            buttonColor='green'
+          >
+            Modifier
+          </Button>
+          <Button
+            icon='account-remove'
+            mode='contained'
+            onPress={() => console.log('Pressed')}
+            buttonColor='red'
+          >
+            Supprimer
+          </Button>
+        </View>
+      </Card>
     </View>
   )
 }
@@ -20,7 +59,16 @@ export default Profile
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    paddingHorizontal: 18,
+    marginTop: 20,
+  },
+  cardProfile: {
+    padding: 15,
+  },
+  controls: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
   },
 })
