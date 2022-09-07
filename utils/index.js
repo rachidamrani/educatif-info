90c0d9be905ebb6984eb01b6e06447b1737b46a6
rachidamrani <rachid3alaoui@gmail.com>
2022-09-08 00:00:31 +0100

    add new colors and regex

--- utils/index.js
+++ utils/index.js
@@ -33,15 +33,23 @@ export function getFormattedDate(date) {
 }
 
 const patterns = {
-  firstname: /^([a-z]{5,10})(\s)?([a-z]{5,10})?$/i,
-  lastname: /^([a-z]{5,10})(\s)?([a-z]{5,10})?$/i,
+  fullname: /^([a-z]{5,10})(\s)([a-z]{5,10})?(\s)?([a-z]{5,10})?$/i,
   level: /[a-z]/i,
-  responsible: /[a-z]/i,
-  dateOfBirth:
+  birthday:
     /(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})/gi,
   phone: /^0(\d\d)(\d\d)(\d\d)(\d\d)(\d)$/,
 }
 
-export function validate(string, field) {
+export function isValid(string, field) {
   return patterns[field].test(string)
+}
+
+export const COLORS = {
+  white: '#fff',
+  black: '#000',
+  blue: '#5D5FEE',
+  grey: '#BABBC3',
+  light: '#F3F4FB',
+  darkBlue: '#7978B5',
+  red: 'red',
 }
