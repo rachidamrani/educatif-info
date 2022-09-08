6566c8989383d218b188dff978e68d4e10efe859
rachidamrani <rachid3alaoui@gmail.com>
2022-09-08 11:58:08 +0100

    add dropdown to select levels

---
+++ components/LevelsDropdown.js
@@ -0,0 +1,40 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+
+import DropDownPicker from 'react-native-dropdown-picker'
+import { COLORS, dropDownLevels } from '../utils'
+
+const LevelsDropdown = () => {
+  const [open, setOpen] = useState(false)
+  const [value, setValue] = useState(null)
+  const [items, setItems] = useState(dropDownLevels)
+
+  console.log(value)
+
+  return (
+    <>
+      <Text style={styles.label}>Niveau</Text>
+      <DropDownPicker
+        placeholder='Choisir un niveau'
+        listMode='SCROLLVIEW'
+        style={{ marginVertical: 10 }}
+        open={open}
+        value={value}
+        items={items}
+        setOpen={setOpen}
+        setValue={setValue}
+        setItems={setItems}
+      />
+    </>
+  )
+}
+
+export default LevelsDropdown
+
+const styles = StyleSheet.create({
+  label: {
+    marginRight: 5,
+    fontSize: 16,
+    color: COLORS.grey,
+  },
+})
