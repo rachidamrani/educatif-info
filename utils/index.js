fd73c52ecce3688988988e9d8da3bfa7af44a866
rachidamrani <rachid3alaoui@gmail.com>
2022-09-08 12:00:41 +0100

    add utilities

--- utils/index.js
+++ utils/index.js
@@ -36,7 +36,7 @@ const patterns = {
   fullname: /^([a-z]{5,10})(\s)([a-z]{5,10})?(\s)?([a-z]{5,10})?$/i,
   level: /[a-z]/i,
   birthday:
-    /(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})/gi,
+    /(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})/,
   phone: /^0(\d\d)(\d\d)(\d\d)(\d\d)(\d)$/,
 }
 
@@ -53,3 +53,23 @@ export const COLORS = {
   darkBlue: '#7978B5',
   red: 'red',
 }
+
+export const dropDownLevels = [
+  { label: 'Primaire', value: 'pri' },
+  { label: '6ère année', value: '6ème', parent: 'pri' },
+  { label: '5ère année', value: '5ème', parent: 'pri' },
+  { label: '4ère année', value: '4ème', parent: 'pri' },
+  { label: '3ère année', value: '3ème', parent: 'pri' },
+  { label: '2ère année', value: '2ème', parent: 'pri' },
+  { label: '1ère année', value: '1ère', parent: 'pri' },
+
+  { label: 'Collège', value: 'na' },
+  { label: '3AC', value: '3AC', parent: 'na' },
+  { label: '2AC', value: '2AC', parent: 'na' },
+  { label: '1AC', value: '1AC', parent: 'na' },
+
+  { label: 'Lycée', value: 'ly' },
+  { label: 'TC', value: 'TSC', parent: 'ly' },
+  { label: '1BAC', value: '1BACSE', parent: 'ly' },
+  { label: '2BAC', value: '2BACSEC', parent: 'ly' },
+]
