7278699b2eeaa8626e8c098d61365983178765ca
rachidamrani <rachid3alaoui@gmail.com>
2022-09-08 12:00:15 +0100

    delete dummy data

--- store/adherents/adherentSlice.js
+++ store/adherents/adherentSlice.js
@@ -2,48 +2,7 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const adherentSlice = createSlice({
   name: 'adherent',
-  initialState: [
-    // {
-    //   firstname: 'Rachid',
-    //   lastname: 'Amrani',
-    //   level: 'TSC',
-    //   responsible: 'Père',
-    //   dateOfBirth: '1991-05-05',
-    //   phone: '0666911974',
-    //   registrationDate: 'Sat Sep 03 2022 17:40:34 GMT+0100 (GMT+01:00)',
-    //   id: '1',
-    // },
-    // {
-    //   firstname: 'Halima',
-    //   lastname: 'Alaoui',
-    //   level: 'TSC',
-    //   responsible: 'Mère',
-    //   dateOfBirth: '1997-05-05',
-    //   phone: '0666911974',
-    //   registrationDate: 'Sat Sep 03 2022 17:41:16 GMT+0100 (GMT+01:00)',
-    //   id: '2',
-    // },
-    // {
-    //   firstname: 'Jalila',
-    //   lastname: 'Amraoui',
-    //   level: '1ér bac Scientifique',
-    //   responsible: 'Père',
-    //   dateOfBirth: '1995-05-05',
-    //   phone: '0666911974',
-    //   registrationDate: 'Sat Sep 03 2022 17:41:57 GMT+0100 (GMT+01:00)',
-    //   id: '3',
-    // },
-    // {
-    //   firstname: 'Mohamed',
-    //   lastname: 'Amine',
-    //   level: '1ér bac Scientifique',
-    //   responsible: 'Père',
-    //   dateOfBirth: '1995-05-05',
-    //   phone: '0666911974',
-    //   registrationDate: 'Sat Sep 03 2022 17:48:34 GMT+0100 (GMT+01:00)',
-    //   id: '4',
-    // },
-  ],
+  initialState: [],
   reducers: {
     addNewAdherent: (state, action) => {
       state.push(action.payload)
