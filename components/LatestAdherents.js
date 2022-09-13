115be559019b2b633ccb7b3f053e62bfcba0912d
rachidamrani <rachid3alaoui@gmail.com>
2022-09-13 09:49:18 +0100

    add limit of displayed adherents

--- components/LatestAdherents.js
+++ components/LatestAdherents.js
@@ -3,12 +3,14 @@ import { View } from 'native-base'
 import { useSelector } from 'react-redux'
 import AdherentItem from './AdherentItem'
 
-const LatestAdherents = ({ edit }) => {
+const LatestAdherents = ({ edit, limit }) => {
   const { adherentsList } = useSelector((state) => state.adherents)
 
   return (
     <FlatList
-      data={adherentsList.slice().reverse().slice(0, 8)}
+      data={
+        limit ? adherentsList.slice().reverse().slice(0, limit) : adherentsList
+      }
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => <AdherentItem adherent={item} edit={edit} />}
       showsVerticalScrollIndicator={false}
